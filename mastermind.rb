708d7a241e07c67362c62ed39b661789f73c70c8
Den <tulipadanys@gmail.com>
2022-06-21 10:25:02 +0300

    Update mastermind.rb
    Working project

--- mastermind.rb
+++ mastermind.rb
@@ -5,51 +5,48 @@ class Game
     @num_colors = num_colors
     @color_scheme = COLORS.sample(num_colors)
     @moves_left = num_moves
+    @player_guess = Array.new(num_colors, 0)
   end
 
-  def start
+  def start_game
     puts "Colors to choose from #{COLORS}"
-    end_game = false
 
-    until end_game
+    until end_game?
       puts "Enter #{@num_colors} from the list above separated with ', ' (coma and space):"
       @player_selection = gets.chomp.split(', ')
-      @player_guess = Array.new(@num_colors, 0)
 
       @player_selection.each_with_index do |color, i|
-        if color == COLORS[i]
+        if color == @color_scheme[i]
           puts "You have put #{color} in the right place."
           @player_guess[i] = 1
-        elsif !COLORS.find_index(color).nil?
+        elsif !@color_scheme.find_index(color).nil?
           puts "You have put #{color} in the wrong place."
-          @player_guess[i] = 1
+          @player_guess[i] = -1
         else
           puts "The #{color} is not present."
           @player_guess[i] = 0
-        end 
+        end
       end
 
       @moves_left -= 1
-      end_gane = check_end_game
-
     end
   end
 
   private
 
-  def check_end_game
+  def end_game?
     if @num_colors == @player_guess.reduce{ |sum,e| sum + e }
       puts 'Congrats! You won!'
       return true
-    elsif self.moves_left == 0
+    elsif @moves_left.zero?
       puts 'You couldn\'t crack the code. You lost!'
       return true
-    else 
+    else
       return false
     end
   end
 
 end
 
-game = Game.new()
-game.start
+game = Game.new
+game.start_game

0391274bf3369748cc4e489913edecf933b8b10e
Den <tulipadanys@gmail.com>
2022-06-21 11:24:22 +0300

    Update mastermind.rb
    Changed user response from array to hash. Changed output style.

--- mastermind.rb
+++ mastermind.rb
@@ -5,37 +5,42 @@ class Game
     @num_colors = num_colors
     @color_scheme = COLORS.sample(num_colors)
     @moves_left = num_moves
-    @player_guess = Array.new(num_colors, 0)
+    @player_guess = {}
   end
 
   def start_game
     puts "Colors to choose from #{COLORS}"
+    puts 'Legend: X - color not present, O - color is in the wrong place, + - color is in the correct place.'
 
     until end_game?
-      puts "Enter #{@num_colors} from the list above separated with ', ' (coma and space):"
-      @player_selection = gets.chomp.split(', ')
+      puts "Enter #{@num_colors} color(s) from the list above separated with ', ' (coma and space):"
+      player_selection = gets.chomp.split(', ')
+      @player_guess = {}
 
-      @player_selection.each_with_index do |color, i|
+      puts player_selection.join(' | ')
+
+      player_selection.each_with_index do |color, i|
         if color == @color_scheme[i]
-          puts "You have put #{color} in the right place."
-          @player_guess[i] = 1
+          @player_guess[color] = 1
         elsif !@color_scheme.find_index(color).nil?
-          puts "You have put #{color} in the wrong place."
-          @player_guess[i] = -1
+          @player_guess[color] = -1
         else
-          puts "The #{color} is not present."
-          @player_guess[i] = 0
+          @player_guess[color] = 0
         end
       end
 
+      puts show_player_guess
+
       @moves_left -= 1
+
+      puts '===================='
     end
   end
 
   private
 
   def end_game?
-    if @num_colors == @player_guess.reduce{ |sum,e| sum + e }
+    if @num_colors == @player_guess.values.reduce { |sum, e| sum + e }
       puts 'Congrats! You won!'
       return true
     elsif @moves_left.zero?
@@ -46,6 +51,18 @@ class Game
     end
   end
 
+  def show_player_guess
+    @player_guess.map { |color, status|
+      case status
+      when 1
+        '+'.prepend(' ' * (color.length/2)) + ' ' * (color.length.odd? ? color.length/2 : color.length/2 - 1)
+      when -1
+        'O'.prepend(' ' * (color.length/2)) + ' ' * (color.length.odd? ? color.length/2 : color.length/2 - 1)
+      when 0
+        'X'.prepend(' ' * (color.length/2)) + ' ' * (color.length.odd? ? color.length/2 : color.length/2 - 1)
+      end
+    }.join(' | ')
+  end
 end
 
 game = Game.new

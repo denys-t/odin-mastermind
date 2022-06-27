8df7c5b11e0eab660dac062c7e0dfff871f4b158
Den <tulipadanys@gmail.com>
2022-06-27 11:38:07 +0300

    Update mastermind.rb
    Player role added to initialize method. Started creating computer strategy by Knuth's method. Added method to generate all patterns

--- mastermind.rb
+++ mastermind.rb
@@ -1,14 +1,29 @@
 class Game
   COLORS = ['red', 'green', 'blue', 'yellow', 'purple', 'orange', 'pink', 'cyan']
 
-  def initialize(num_colors = 4, num_moves = 12)
+  def initialize(player_role = 'codebreaker', num_colors = 4, num_moves = 12)
     @num_colors = num_colors
-    @color_scheme = COLORS.sample(num_colors)
     @moves_left = num_moves
     @player_guess = {}
+    @player_role = player_role
+    @code_pool = []
   end
 
   def start_game
+    case @player_role
+    when 'codebreaker'
+      game_codebreaker
+    when 'codemaker'
+      generate_all_codes
+      game_codemaker
+    end
+  end
+
+  private
+
+  def game_codebreaker
+    @color_scheme = COLORS.sample(@num_colors)
+
     puts "Colors to choose from #{COLORS}"
     puts 'Legend: X - color not present, O - color is in the wrong place, + - color is in the correct place.'
 
@@ -35,9 +50,25 @@ class Game
 
       puts '===================='
     end
+
+    def game_codemaker
+      puts "Colors to choose from #{COLORS}"
+      puts "Create a color scheme of #{@num_colors}, separated by \", \" (coma and space)."
+
+      @color_scheme = gets.chomp.split(', ')
+
+      until end_game?
+
+      end
+    end
   end
 
-  private
+  def generate_all_codes
+    (0..@num_colors ** COLORS.length - 1).each do |i|
+      code = i.to_s(COLORS.length)
+      @code_pool[i] = (code.length == @num_colors ? code.split('') : code.prepend('0' * (@num_colors - code.length)).split(''))
+    end
+  end
 
   def end_game?
     if @num_colors == @player_guess.values.reduce { |sum, e| sum + e }

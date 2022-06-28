40ca7f737990a65aa2acd1d676fea75464dec52a
Den <tulipadanys@gmail.com>
2022-06-28 11:04:17 +0300

    Update mastermind.rb
    Fixed constant declaration. Cahnged some variable names to depict that computer also can play

--- mastermind.rb
+++ mastermind.rb
@@ -1,12 +1,11 @@
 class Game
-  COLORS = ['red', 'green', 'blue', 'yellow', 'purple', 'orange', 'pink', 'cyan']
-  
+  COLORS = %w[red green blue yellow purple orange pink cyan].freeze
 
   def initialize(player_role = 'codebreaker', num_colors = 4, num_moves = 12)
     @num_colors = num_colors
     @moves_left = num_moves
-    @player_guess = {}
-    @player_role = player_role
+    @codebreaker_guess = {}
+    @player_role = player_role # 'codebreaker' or 'codemaker'
     @code_pool = []
   end
 
@@ -31,46 +30,46 @@ class Game
     until end_game?
       puts "Enter #{@num_colors} color(s) from the list above separated with \", \" (coma and space):"
       player_selection = gets.chomp.split(', ')
-      @player_guess = {}
+      @codebreaker_guess = {}
 
       player_selection.each_with_index do |color, i|
         if color == @color_scheme[i]
-          @player_guess[color] = 1
+          @codebreaker_guess[color] = 1
         elsif !@color_scheme.find_index(color).nil?
-          @player_guess[color] = -1
+          @codebreaker_guess[color] = -1
         else
-          @player_guess[color] = 0
+          @codebreaker_guess[color] = 0
         end
       end
 
-      puts show_player_guess(player_selection)
+      puts show_codebreaker_guess(player_selection)
 
       @moves_left -= 1
 
       puts '===================='
     end
+  end
 
-    def game_codemaker
-      puts "Colors to choose from #{COLORS}"
-      puts "Create a color scheme of #{@num_colors}, separated by \", \" (coma and space)."
+  def game_codemaker
+    puts "Colors to choose from #{COLORS}"
+    puts "Create a color scheme of #{@num_colors}, separated by \", \" (coma and space)."
 
-      @color_scheme = gets.chomp.split(', ')
+    @color_scheme = gets.chomp.split(', ')
 
-      until end_game?
+    until end_game?
 
-      end
     end
   end
 
   def generate_all_codes
     (0..@num_colors ** COLORS.length - 1).each do |i|
       code = i.to_s(COLORS.length)
-      @code_pool[i] = (code.length == @num_colors ? code.split('') : code.prepend('0' * (@num_colors - code.length)).split(''))
+      @code_pool[i] = (code.length == @num_colors ? code.split('') : code.prepend('0' * (@num_colors - code.length)).split('')).map { |str| str.to_i}
     end
   end
 
   def end_game?
-    if @num_colors == @player_guess.values.reduce { |sum, e| sum + e }
+    if @num_colors == @codebreaker_guess.values.reduce(0) { |sum, e| sum + e }
       puts 'Congrats! You won!'
       return true
     elsif @moves_left.zero?
@@ -81,19 +80,19 @@ class Game
     end
   end
 
-  def show_player_guess(player_selection)
-    result = player_selection.join(' | ') + 
-            ' ===> ' +
-            @player_guess.map { |color, status|
-              case status
-              when 1
-                'X'
-              when -1
-                'O'
-              when 0
-                ' '
-              end
-            }.join(' | ')
+  def show_codebreaker_guess(player_selection)
+    player_selection.join(' | ') + 
+      ' ===> ' +
+      @codebreaker_guess.map { |_color, status|
+        case status
+        when 1
+          'X'
+        when -1
+          'O'
+        when 0
+          ' '
+        end
+      }.shuffle.join(' | ')
   end
 end
 

827d035dd266d7e2959987417d33e1729addfe99
Den <tulipadanys@gmail.com>
2022-06-29 12:15:44 +0300

    Update mastermind.rb
    Working on Knuth's algorithm.  Changed the way of evaluating codebreaker's guess to contain only the number of colors in the right and wrong place.

--- mastermind.rb
+++ mastermind.rb
@@ -24,23 +24,14 @@ class Game
   def game_codebreaker
     @color_scheme = COLORS.sample(@num_colors)
 
-    puts "Colors to choose from #{COLORS}"
-    puts 'Legend: " " (space) - color is not present, O - color is in the wrong place, X - color is in the correct place.'
+    puts "Colors to choose from #{COLORS}. No duplicates allowed."
+    puts 'Legend: " " (space) - color is not present, O - color is in the wrong place, X - color is in the right place.'
 
     until end_game?
-      puts "Enter #{@num_colors} color(s) from the list above separated with \", \" (coma and space):"
-      player_selection = gets.chomp.split(', ')
-      @codebreaker_guess = {}
+      puts "Enter #{@num_colors} color(s) from the list above separated with \" \" (one space):"
+      player_selection = gets.chomp.split(' ')
 
-      player_selection.each_with_index do |color, i|
-        if color == @color_scheme[i]
-          @codebreaker_guess[color] = 1
-        elsif !@color_scheme.find_index(color).nil?
-          @codebreaker_guess[color] = -1
-        else
-          @codebreaker_guess[color] = 0
-        end
-      end
+      check_player_selection(player_selection)
 
       puts show_codebreaker_guess(player_selection)
 
@@ -51,29 +42,73 @@ class Game
   end
 
   def game_codemaker
-    puts "Colors to choose from #{COLORS}"
-    puts "Create a color scheme of #{@num_colors}, separated by \", \" (coma and space)."
+    puts "Colors to choose from #{COLORS}. No duplicates allowed."
+    puts "Create a color scheme of #{@num_colors}, separated by \" \" (one space)."
 
-    @color_scheme = gets.chomp.split(', ')
+    @color_scheme = gets.chomp.split(' ')
+    @color_scheme_code = @color_scheme.map { |color| COLORS.index(color) }
+
+    player_selection = []
+
+    (0..3).each { |i| player_selection[i] = COLORS[i] }
+
+    check_player_selection(player_selection)
+
+    puts show_codebreaker_guess(player_selection)
 
     until end_game?
+      filter_code_pool(player_selection)
+    end
+  end
 
+  def filter_code_pool(player_selection)
+    selected_code = player_selection.map { |color| COLORS.index(color) }
+
+    @code_pool.delete(selected_code)
+
+    s = []
+    @code_pool.each do |code|
+      guess_score = { 'X' => 0, 'O' => 0 }
+      selected_code.each_with_index do |i, e|
+        if e == code[i]
+          guess_score['X'] += 1
+        elsif !code.find_index(e).nil?
+          guess_score['O'] += 1
+        end
+      end
+
+      if guess_score == @codebreaker_guess
+        s.push(code)
+      end
+    end
+
+  end
+
+  def check_player_selection(player_selection)
+    @codebreaker_guess = { 'X' => 0, 'O' => 0 }
+
+    player_selection.each_with_index do |color, i|
+      if color == @color_scheme[i]
+        @codebreaker_guess['X'] += 1
+      elsif !@color_scheme.find_index(color).nil?
+        @codebreaker_guess['O'] += 1
+      end
     end
   end
 
   def generate_all_codes
-    (0..@num_colors ** COLORS.length - 1).each do |i|
+    (0..COLORS.length ** @num_colors - 1).each do |i|
       code = i.to_s(COLORS.length)
       @code_pool[i] = (code.length == @num_colors ? code.split('') : code.prepend('0' * (@num_colors - code.length)).split('')).map { |str| str.to_i}
     end
   end
 
   def end_game?
-    if @num_colors == @codebreaker_guess.values.reduce(0) { |sum, e| sum + e }
-      puts 'Congrats! You won!'
+    if @num_colors == @codebreaker_guess['X']
+      puts "Congrats! #{@player_role == 'codebreaker' ? 'You' : 'Computer'} won!"
       return true
     elsif @moves_left.zero?
-      puts 'You couldn\'t crack the code. You lost!'
+      puts "#{@player_role == 'codebreaker' ? 'You' : 'Computer'} couldn\'t crack the code. #{@player_role == 'codebreaker' ? 'Computer' : 'You'} won!"
       return true
     else
       return false
@@ -83,18 +118,12 @@ class Game
   def show_codebreaker_guess(player_selection)
     player_selection.join(' | ') + 
       ' ===> ' +
-      @codebreaker_guess.map { |_color, status|
-        case status
-        when 1
-          'X'
-        when -1
-          'O'
-        when 0
-          ' '
-        end
-      }.shuffle.join(' | ')
+      @codebreaker_guess.map { |key, value|
+        (1..value).map { key }
+      }.reduce { |sum, e| sum.concat(e) }.shuffle.join(' | ')
   end
 end
 
-game = Game.new
+#game = Game.new('codemaker')
+game = Game.new()
 game.start_game

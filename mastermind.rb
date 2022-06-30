8507eacb5bd59db9a834acf333580277ca11bf31
Den <tulipadanys@gmail.com>
2022-06-30 13:01:12 +0300

    Update mastermind.rb
    Changed code so the duplicates are allowed. Finished coding Knuth's algorithm, needs testing

--- mastermind.rb
+++ mastermind.rb
@@ -22,19 +22,22 @@
     ################# CODE-BREAKER #################
 
     def game_codebreaker
-      @color_scheme = COLORS.sample(@num_colors)
+      @color_scheme = @num_colors.times.map {COLORS.sample}
+      p @color_scheme
+     
+      #@color_scheme = ['red', 'green', 'blue', 'yellow']
       @color_scheme_code = @color_scheme.map { |color| COLORS.index(color) }
 
-      puts "Colors to choose from #{COLORS}. No duplicates allowed."
+      puts "Colors to choose from #{COLORS}. Duplicates are allowed."
+      puts "Enter #{@num_colors} color(s) from the list above separated with \" \" (one space):"
       puts 'Legend: " " (space) - color is not present, O - color is in the wrong place, X - color is in the right place.'
 
       until end_game?
         puts "===== Turns left: #{@turns_left} ====="
-        puts "Enter #{@num_colors} color(s) from the list above separated with \" \" (one space):"
         player_selection = gets.chomp.split(' ')
         player_selection_code = player_selection.map { |color| COLORS.index(color) }
 
-        check_player_selection(player_selection_code, @color_scheme_code)
+        get_feedback(player_selection_code, @color_scheme_code)
 
         puts show_codebreaker_guess(player_selection)
 
@@ -45,28 +48,27 @@
     ################# CODE-MAKER #################
 
     def game_codemaker
-      generate_all_codes
-      knuth_codes = @all_codes
-      possible_codes = @all_codes
+      generate_all_codes # @all_codes
+      knuth_codes = Array.new(@all_codes)
+      possible_codes = Array.new(@all_codes)
       
-      puts "Colors to choose from #{COLORS}. No duplicates allowed."
+      puts "Colors to choose from #{COLORS}. Duplicates are allowed."
       puts "Create a color scheme of #{@num_colors}, separated by \" \" (one space)."
 
       @color_scheme = gets.chomp.split(' ')
       @color_scheme_code = @color_scheme.map { |color| COLORS.index(color) }
 
-      player_selection = []
+      player_selection_code = [0, 0, 1, 1] # choosing the first guess
+      player_selection = player_selection_code.map { |color_code| COLORS[color_code] }
 
-      (0..3).each { |i| player_selection[i] = COLORS[i] } # choosing the first guess
-      player_selection_code = player_selection.map { |color| COLORS.index(color) }
-
-      check_player_selection(player_selection_code, @color_scheme_code)
+      get_feedback(player_selection_code, @color_scheme_code) # @codebreaker_guess
 
       puts show_codebreaker_guess(player_selection)
 
       until end_game?
-        #filter_code_pool(player_selection)
-
+        possible_codes.delete(player_selection_code)
+        prune_list(player_selection_code, knuth_codes)
+        player_selection_code = get_new_code(knuth_codes, possible_codes)
       end
     end
 
@@ -78,41 +80,72 @@
       end
     end
 
-    # def filter_code_pool(player_selection)
-    #   selected_code = player_selection.map { |color| COLORS.index(color) }
+    def prune_list(last_guess_code, knuth_codes)
+      knuth_codes.each do |code|
+        retrieved_feedback = get_feedback(code, last_guess_code)
+        knuth_codes.delete(code) if retrieved_feedback != @codebreaker_guess
+      end
+    end
 
-    #   @code_pool.delete(selected_code)
+    def get_new_code(knuth_codes, possible_codes)
+      guess_codes = minimax(knuth_codes, possible_codes)
+      code = get_guess_code_from_list(knuth_codes, guess_codes)
+    end
 
-    #   s = []
-    #   @code_pool.each do |code|
-    #     guess_score = { 'X' => 0, 'O' => 0 }
-    #     selected_code.each_with_index do |i, e|
-    #       if e == code[i]
-    #         guess_score['X'] += 1
-    #         code[i] = 'X'
-    #       elsif !code.find_index(e).nil?
-    #         guess_score['O'] += 1
-    #         code[code.find_index(e)] = 'X'
-    #       end
-    #     end
+    def minimax(knuth_codes, possible_codes)
+      scores = {}
 
-    #     if guess_score == @codebreaker_guess
-    #       s.push(code)
-    #     end
-    #   end
+      possible_codes.each do |code|
+        times_found = Hash.new(0)
 
-    # end
+        knuth_codes.each do |code_to_crack|
+          feedback = get_feedback(code_to_crack, code)
+          times_found[feedback_to_string(feedback)] += 1
+        end
+
+        maximum = times_found.values.max
+        scores[code] = maximum
+      end
+
+      minimum = scores.values.min
+      guess_codes = []
+
+      possible_codes.each do |code|
+        guess_codes.append(code) if scores[code] == minimum
+      end
+
+      return guess_codes
+    end
+
+    def feedback_to_string(feedback)
+      feedback.map { |key, value|
+        (1..value).map { key }
+      }.reduce { |sum, e| sum.concat(e) }.sort.join
+    end
+
+    def get_guess_code_from_list(knuth_codes, guess_codes)
+      knuth_codes.each do |code|
+        if !guess_codes.find_index(code).nil?
+          return code
+        end
+      end
+
+      return guess_codes[0]
+    end
 
     ################# COMMON #################
 
-    def check_player_selection(player_selection_code, code)
+    def get_feedback(player_selection_code, code)
       @codebreaker_guess = { 'X' => 0, 'O' => 0 }
+      tested_code = Array.new(code)
 
       player_selection_code.each_with_index do |color, i|
-        if color == code[i]
+        if color == tested_code[i]
           @codebreaker_guess['X'] += 1
-        elsif !code.find_index(color).nil?
+          tested_code[i] = 'X'
+        elsif !tested_code.find_index(color).nil?
           @codebreaker_guess['O'] += 1
+          tested_code[tested_code.find_index(color)] = 'X'
         end
       end
     end

873220c4d320d224f41382d947c8b834c9cc1f23
Den <tulipadanys@gmail.com>
2022-07-01 12:38:28 +0300

    Update mastermind.rb
    Fixed minor bugs. Knuth algorithm not working

--- mastermind.rb
+++ mastermind.rb
@@ -1,14 +1,16 @@
   class Game
-    COLORS = %w[red green blue yellow purple orange pink cyan].freeze
+    COLORS = %w[red green blue orange pink cyan].freeze
 
     def initialize(player_role = 'codebreaker', num_colors = 4, num_turns = 12)
       @num_colors = num_colors
-      @turns_left = num_turns
+      @num_turns = num_turns
       @codebreaker_guess = {}
       @player_role = player_role # 'codebreaker' or 'codemaker'
     end
 
     def start_game
+      @turns_left = @num_turns
+
       case @player_role
       when 'codebreaker'
         game_codebreaker
@@ -23,9 +25,6 @@
 
     def game_codebreaker
       @color_scheme = @num_colors.times.map {COLORS.sample}
-      p @color_scheme
-     
-      #@color_scheme = ['red', 'green', 'blue', 'yellow']
       @color_scheme_code = @color_scheme.map { |color| COLORS.index(color) }
 
       puts "Colors to choose from #{COLORS}. Duplicates are allowed."
@@ -37,7 +36,7 @@
         player_selection = gets.chomp.split(' ')
         player_selection_code = player_selection.map { |color| COLORS.index(color) }
 
-        get_feedback(player_selection_code, @color_scheme_code)
+        @codebreaker_guess = get_feedback(player_selection_code, @color_scheme_code)
 
         puts show_codebreaker_guess(player_selection)
 
@@ -61,14 +60,22 @@
       player_selection_code = [0, 0, 1, 1] # choosing the first guess
       player_selection = player_selection_code.map { |color_code| COLORS[color_code] }
 
-      get_feedback(player_selection_code, @color_scheme_code) # @codebreaker_guess
+      @codebreaker_guess = get_feedback(player_selection_code, @color_scheme_code)
 
+      puts "===== Turns left: #{@turns_left} ====="
       puts show_codebreaker_guess(player_selection)
 
       until end_game?
+        @turns_left -= 1
+        puts "===== Turns left: #{@turns_left} ====="
+
         possible_codes.delete(player_selection_code)
         prune_list(player_selection_code, knuth_codes)
         player_selection_code = get_new_code(knuth_codes, possible_codes)
+        player_selection = player_selection_code.map { |color_code| COLORS[color_code] }
+        @codebreaker_guess = get_feedback(player_selection_code, @color_scheme_code)
+        
+        puts show_codebreaker_guess(player_selection)
       end
     end
 
@@ -90,6 +97,7 @@
     def get_new_code(knuth_codes, possible_codes)
       guess_codes = minimax(knuth_codes, possible_codes)
       code = get_guess_code_from_list(knuth_codes, guess_codes)
+      code
     end
 
     def minimax(knuth_codes, possible_codes)
@@ -100,18 +108,19 @@
 
         knuth_codes.each do |code_to_crack|
           feedback = get_feedback(code_to_crack, code)
-          times_found[feedback_to_string(feedback)] += 1
+          feedback_str = feedback_to_string(feedback)
+          times_found[feedback_str] += 1 if !feedback_str.to_s.strip.empty?
         end
 
         maximum = times_found.values.max
-        scores[code] = maximum
+        scores[code] = (maximum.nil? ? 0 : maximum)
       end
 
       minimum = scores.values.min
       guess_codes = []
 
       possible_codes.each do |code|
-        guess_codes.append(code) if scores[code] == minimum
+        guess_codes.push(code) if scores[code] == minimum
       end
 
       return guess_codes
@@ -136,18 +145,20 @@
     ################# COMMON #################
 
     def get_feedback(player_selection_code, code)
-      @codebreaker_guess = { 'X' => 0, 'O' => 0 }
+      feedback = { 'X' => 0, 'O' => 0 }
       tested_code = Array.new(code)
 
       player_selection_code.each_with_index do |color, i|
         if color == tested_code[i]
-          @codebreaker_guess['X'] += 1
+          feedback['X'] += 1
           tested_code[i] = 'X'
         elsif !tested_code.find_index(color).nil?
-          @codebreaker_guess['O'] += 1
+          feedback['O'] += 1
           tested_code[tested_code.find_index(color)] = 'X'
         end
       end
+
+      feedback
     end
 
     def end_game?
@@ -172,6 +183,6 @@
     end
   end
 
-  #game = Game.new('codemaker')
-  game = Game.new()
+  game = Game.new('codemaker')
+  #game = Game.new()
   game.start_game
